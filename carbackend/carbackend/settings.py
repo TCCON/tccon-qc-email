794fd9bd0763e1d77950048b2fe052abbbd7d037
Joshua Laughner <jlaugh@caltech.edu>
2020-10-02 09:44:34 -0700

    Moved SQL settings path

--- carbackend/carbackend/settings.py
+++ carbackend/carbackend/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': str(BASE_DIR / 'maria.cnf'),
+            'read_default_file': '/var/www/car/info/maria.cnf',
         }
     }
 }
