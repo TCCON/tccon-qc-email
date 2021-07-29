9593dce2498ac29306ed56e7b41e0e6d07a7a444
Joshua Laughner <jllacct119@gmail.com>
2021-07-28 21:04:21 -0700

    Added landing page site list, some style added

--- carbackend/siteinfo/urls.py
+++ carbackend/siteinfo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'siteinfo'
 urlpatterns = [
+    path('', views.SiteInfoList.as_view(), name='index'),
     path('view/<str:site_id>/', views.ViewSiteInfo.as_view(), name='view'),
 ]
--- carbackend/siteinfo/views.py
+++ carbackend/siteinfo/views.py
@@ -6,6 +6,17 @@ from django.shortcuts import render
 import json
 
 
+class SiteInfoList(View):
+    def get(self, request):
+        with open(settings.SITE_INFO_FILE) as f:
+            all_site_info = json.load(f)
+
+        sites = [{'id': k, 'name': v['long_name']} for k, v in all_site_info.items()]
+        sites.sort(key=lambda s: s['id'])
+        # TODO: limit which sites have the "edit" link active based on user permissions
+        return render(request, 'siteinfo/site_list.html', context={'sites': sites})
+
+
 # Create your views here.
 class ViewSiteInfo(View):
     def get(self, request, site_id):
@@ -17,4 +28,5 @@ class ViewSiteInfo(View):
         except KeyError:
             raise Http404('No existing site information for site "{}"'.format(site_id))
 
+        # TODO: ensure that the standard attributes are in a fixed order
         return render(request, 'siteinfo/view_site_info.html', context={'site_id': site_id, 'info': site_info})
