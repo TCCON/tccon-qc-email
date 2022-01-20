3230989cd864f33579060881a02e16489f742538
Joshua Laughner <jlaugh@caltech.edu>
2022-01-19 18:40:19 -0800

    Initial commit - copied code over from qc_plots

---
+++ qc_email/__init__.py
@@ -0,0 +1,1 @@
+from .qc_email import send_email_from_config, send_email_ext, send_email, write_default_config
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from numpy.distutils.core import Extension
+
+froll_ext = Extension(name='froll', sources=['fortran/froll.f'])
+
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(
+            name='tccon_qc_email',
+            description='Email QA/QC plots of TCCON data to GGGBugs',
+            author='Sebastien Roche & Joshua Laughner',
+            author_email='sebastien.roche@mail.utoronto.ca; jlaugh@caltech.edu',
+            version='1.0.0',
+            url='https://bitbucket.org/rocheseb/tccon_qc_plots/',
+            install_requires=[
+                    'matplotlib>=3.3.2',
+                    'netcdf4>=1.5.4',
+                    'numpy',
+                    'pandas',
+                    'pillow',
+                    'pypdf2',
+                    'scipy>=1.6.1',
+                    'tomli>=1.0.4'
+                ],
+            packages=['qc_plots'],
+            ext_modules=[froll_ext],
+            entry_points={'console_scripts': [
+                    'qc_plots=qc_plots.__main__:main'
+                ]},
+            python_requires='>=3.7'
+    )
