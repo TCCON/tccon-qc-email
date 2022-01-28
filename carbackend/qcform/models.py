07162f357f19208529abd36d1f5c4095ee0931ff
Joshua Laughner <jllacct119@gmail.com>
2022-01-27 21:39:07 -0800

    Updated form to match new QC form

--- carbackend/qcform/models.py
+++ carbackend/qcform/models.py
@@ -40,14 +40,14 @@ class ISection:
             },
             {
                 'name': 'Critical quality checks',
-                'questions': self._make_section('timing_', 'pres_err_', 'nans_', 'qc_flags_', 'rolling_xluft_'),
+                'questions': self._make_section('pres_err_', 'nans_', 'qc_flags_', 'timing_', 'rolling_xluft_'),
                 'intro': """Data with any of the issues here generally requires reprocessing or release flagging (i.e.
 spectra to be flagged on tccondata.org and withheld from the public files). If an issue here
 does not require reprocessing or flagging, please describe why in the comments."""
             },
             {
                 'name': 'Informational quality checks',
-                'questions': self._make_section('lse_', 'sg_', 'nonlin_'),
+                'questions': self._make_section('hcl_vsf_', 'o2_fs_', 'nonlin_', 'sg_', 'lse_'),
                 'intro': """The checks in this section are generally ones that cannot be fixed except in special cases
 and so do not require reprocessing. If the error is sufficiently large, the data may be flagged out."""
             },
@@ -77,16 +77,26 @@ and so do not require reprocessing. If the error is sufficiently large, the data
         fields = {
             'mcs': [],
             'whens': [],
+            'whens_type': 'none',
             'cmts': [],
             'message': mark_safe(self._get_question_message(prefix)),
             'title': self._get_question_title(prefix)
         }
 
+        whens_single_date = False
+        whens_paired_dates = False
         for key, field in self._iter_fields_with_prefixes(prefix):
-            m = re.search(r'(.+_when[0-9]+)s$', key)
+            m = re.search(r'(.+_when[0-9]+)s?$', key)
             if m:
-                key2 = m.group(1) + 'e'
-                fields['whens'].append((field, self._get_field_by_key(key2)))
+                if key.endswith('s'):
+                    key2 = m.group(1) + 'e'
+                    fields['whens'].append((field, self._get_field_by_key(key2)))
+                    whens_paired_dates = True
+                    print('paired', key, prefix)
+                else:
+                    fields['whens'].append((field, None))
+                    whens_single_date = True
+                    print('single', key, prefix)
             elif re.search(r'when[0-9]+e$', key):
                 # We handle the end fields with their paired start fields
                 pass
@@ -94,6 +104,12 @@ and so do not require reprocessing. If the error is sufficiently large, the data
                 fields['cmts'].append(field)
             else:
                 fields['mcs'].append(field)
+
+        if whens_paired_dates:
+            fields['whens_type'] = 'paired'
+        elif whens_single_date:
+            fields['whens_type'] = 'single'
+        print(prefix, 'whens_type is', fields['whens_type'], '\n')
         return fields
 
     @classmethod
@@ -117,8 +133,9 @@ class QCReport(models.Model, ISection):
     # I decided against it because I don't want to deal with issues if
     # we remove a user.
     reviewer = models.CharField(max_length=128, verbose_name='Reviewer')
-    site = models.CharField(max_length=2, choices=utils.get_sites_as_choices())
-    netcdf_files = models.TextField()
+    site = models.CharField(max_length=2, choices=utils.get_sites_as_choices(), verbose_name='Site')
+    netcdf_files = models.TextField(verbose_name='NetCDF files')
+    modification_time = models.DateTimeField(auto_now=True)
 
     timing_title = 'Timing error'
     timing_message = 'Check at least the Xluft PM - AM plot (|∆| < 0.01 ideal) and Xluft vs. SZA plots.'
@@ -144,12 +161,12 @@ class QCReport(models.Model, ISection):
     timing_when8e = models.DateField(blank=True, null=True, verbose_name='End of ninth timing error')
     timing_when9s = models.DateField(blank=True, null=True, verbose_name='Start of tenth timing error')
     timing_when9e = models.DateField(blank=True, null=True, verbose_name='End of tenth timing error')
-    timing_cmts = models.TextField(blank=True)
+    timing_cmts = models.TextField(blank=True, verbose_name='Comments')
 
     pres_err_title = 'Pressure sensor error'
     pres_err_message = 'Check the zmin - zobs plot (< 1 hPa ideal, < 3 hPa acceptable) if available. If not, check pout for drift.'
-    pres_err_present = models.CharField(max_length=1, choices=_choices_ynu)
-    pres_err_reproc = models.CharField(max_length=1, choices=_choices_ynms)
+    pres_err_present = models.CharField(max_length=1, choices=_choices_ynu, verbose_name='Is pressure error present')
+    pres_err_reproc = models.CharField(max_length=1, choices=_choices_ynms, verbose_name='Requires reprocessing')
     pres_err_when0s = models.DateField(blank=True, null=True)
     pres_err_when0e = models.DateField(blank=True, null=True)
     pres_err_when1s = models.DateField(blank=True, null=True)
@@ -170,24 +187,24 @@ class QCReport(models.Model, ISection):
     pres_err_when8e = models.DateField(blank=True, null=True)
     pres_err_when9s = models.DateField(blank=True, null=True)
     pres_err_when9e = models.DateField(blank=True, null=True)
-    pres_err_cmts = models.TextField(blank=True)
+    pres_err_cmts = models.TextField(blank=True, verbose_name='Comments')
 
     nans_title = 'Window NaNs (Non-Voigt compiler check)'
     nans_message = 'Check the bar graph of NaNs per window; if some windows are all NaNs and others not, this indicates a compiler problem. Common offenders are the CH4 5938 wCO2 windows.'
-    nans_present = models.CharField(max_length=1, choices=_choices_yn)
-    nans_reproc = models.CharField(max_length=1, choices=_choices_ynms)
-    nans_cmts = models.TextField(blank=True)
+    nans_present = models.CharField(max_length=1, choices=_choices_yn, verbose_name='Mismatched numbers of NaNs')
+    nans_reproc = models.CharField(max_length=1, choices=_choices_ynms, verbose_name='Required reprocessing')
+    nans_cmts = models.TextField(blank=True, verbose_name='Comments (include which windows are affected)')
 
     qc_flags_title = 'Automatic QC flags'
     qc_flags_message = 'Check the bar graph of flags removing spectra for unusual flags removing significant numbers of spectra. xhf_error can remove more than it should at wet sites in particular.'
-    qc_flags_present = models.CharField(max_length=1, choices=_choices_ynu)
-    qc_flags_reproc = models.CharField(max_length=1, choices=_choices_ynms)
-    qc_flags_cmts = models.TextField(blank=True)
+    qc_flags_present = models.CharField(max_length=1, choices=_choices_ynu, verbose_name='Unusual flags present')
+    qc_flags_reproc = models.CharField(max_length=1, choices=_choices_ynms, verbose_name='Required reprocessing')
+    qc_flags_cmts = models.TextField(blank=True, verbose_name='Comments (include which flags are unusual)')
 
     rolling_xluft_title = 'Rolling Xluft check'
     rolling_xluft_message = 'Check the 500 spectra rolling Xluft median for time periods significantly outside the ±0.004 limits'
-    rolling_xluft_present = models.CharField(max_length=1, choices=_choices_ynu)
-    rolling_xluft_flag = models.CharField(max_length=1, choices=_choices_ynu)
+    rolling_xluft_present = models.CharField(max_length=1, choices=_choices_ynu, verbose_name='Out-of-family Xluft present')
+    rolling_xluft_flag = models.CharField(max_length=1, choices=_choices_ynu, verbose_name='Required release flagging')
     rolling_xluft_when0s = models.DateField(blank=True, null=True)
     rolling_xluft_when0e = models.DateField(blank=True, null=True)
     rolling_xluft_when1s = models.DateField(blank=True, null=True)
@@ -208,7 +225,7 @@ class QCReport(models.Model, ISection):
     rolling_xluft_when8e = models.DateField(blank=True, null=True)
     rolling_xluft_when9s = models.DateField(blank=True, null=True)
     rolling_xluft_when9e = models.DateField(blank=True, null=True)
-    rolling_xluft_cmts = models.TextField(blank=True)
+    rolling_xluft_cmts = models.TextField(blank=True, verbose_name='Comments')
 
     lse_title = 'LSE'
     lse_message = """Check the rolling median LSE plot. This should be < 0.0001 or so if the site has two detectors
@@ -219,9 +236,9 @@ and an M16 controller. If it is uniformly zero (i.e. the site has only one detec
 <li>2. If the site has an M15 controller, that the team used the Dohe method to correct ghosts</li>
 <li>3. If the LSE is large, inquire why</li>
 </ul>"""
-    lse_present = models.CharField(max_length=1, choices=_choices_lse)
-    lse_follow_up = models.CharField(max_length=1, choices=_choices_yn)
-    lse_cmts = models.TextField(blank=True)
+    lse_present = models.CharField(max_length=1, choices=_choices_lse, verbose_name='LSE status')
+    lse_follow_up = models.CharField(max_length=1, choices=_choices_yn, verbose_name='Requires follow up')
+    lse_cmts = models.TextField(blank=True, verbose_name='Comments')
 
     sg_title = 'SG stretch'
     sg_message = """Check the rolling median SG stretch plot, considering both the median and individual
@@ -229,7 +246,7 @@ spectra data points. Large deviations in the median, or repeating diurnal variat
 tered on solar noon in the individual values indicative of diurnal cycles > 1 ppm should
 be brought to the PI’s attention, especially if they correspond with deviations in Xluft.
 (These diurnal variations may be difficult to see in long time series.)"""
-    sg_present = models.CharField(max_length=1, choices=_choices_ynu)
+    sg_present = models.CharField(max_length=1, choices=_choices_ynu, verbose_name='SG stretch suggests pointing error')
     sg_when0s = models.DateField(blank=True, null=True)
     sg_when0e = models.DateField(blank=True, null=True)
     sg_when1s = models.DateField(blank=True, null=True)
@@ -250,13 +267,13 @@ be brought to the PI’s attention, especially if they correspond with deviation
     sg_when8e = models.DateField(blank=True, null=True)
     sg_when9s = models.DateField(blank=True, null=True)
     sg_when9e = models.DateField(blank=True, null=True)
-    sg_cmts = models.TextField(blank=True)
+    sg_cmts = models.TextField(blank=True, verbose_name='Comments')
 
     nonlin_title = 'Nonlinearity'
     nonlin_message = """Examine plots of rolling derivative of DIP vs. CL, the DIP vs. CL scatter plot, and the 
 timeseries of DIP itself. Sodankyla 2017 data (with a known nonlinearity) had a DIP vs.
 O2 CL slope of 0.01, such a slope should be near 0 when no nonlinearity is present."""
-    nonlin_present = models.CharField(max_length=1, choices=_choices_ynu)
+    nonlin_present = models.CharField(max_length=1, choices=_choices_ynu, verbose_name='Nonlinearity present')
     nonlin_when0s = models.DateField(blank=True, null=True)
     nonlin_when0e = models.DateField(blank=True, null=True)
     nonlin_when1s = models.DateField(blank=True, null=True)
@@ -277,10 +294,42 @@ O2 CL slope of 0.01, such a slope should be near 0 when no nonlinearity is prese
     nonlin_when8e = models.DateField(blank=True, null=True)
     nonlin_when9s = models.DateField(blank=True, null=True)
     nonlin_when9e = models.DateField(blank=True, null=True)
-    nonlin_cmts = models.TextField(blank=True)
+    nonlin_cmts = models.TextField(blank=True, verbose_name='Comments')
+
+    hcl_vsf_title = 'HCl VSF'
+    hcl_vsf_message = 'Check the HCl VSF plot and verify that the VSF is stable over time. If there are jumps, check if they are related to changes in the instrument.'
+    hcl_vsf_unstable = models.CharField(max_length=1, choices=_choices_ynu, verbose_name='HCl VSF is unstable/jumps')
+    hcl_vsf_follow_up = models.CharField(max_length=1, choices=_choices_yn, verbose_name='Requires follow up')
+    hcl_vsf_when0 = models.DateField(blank=True, null=True)
+    hcl_vsf_when1 = models.DateField(blank=True, null=True)
+    hcl_vsf_when2 = models.DateField(blank=True, null=True)
+    hcl_vsf_when3 = models.DateField(blank=True, null=True)
+    hcl_vsf_when4 = models.DateField(blank=True, null=True)
+    hcl_vsf_when5 = models.DateField(blank=True, null=True)
+    hcl_vsf_when6 = models.DateField(blank=True, null=True)
+    hcl_vsf_when7 = models.DateField(blank=True, null=True)
+    hcl_vsf_when8 = models.DateField(blank=True, null=True)
+    hcl_vsf_when9 = models.DateField(blank=True, null=True)
+    hcl_vsf_cmts = models.TextField(blank=True, verbose_name='Comments')
+
+    o2_fs_title = 'O2 frequency shift'
+    o2_fs_message = 'Check the o2_7885_fs time series. If the values are off the y-axix scale or there are jumps, check with the site to see if these are related to changes in the instrument.'
+    o2_fs_unstable = models.CharField(max_length=1, choices=_choices_ynu, verbose_name='O2 FS is off scale/jumps')
+    o2_fs_follow_up = models.CharField(max_length=1, choices=_choices_yn, verbose_name='Requires follow up')
+    o2_fs_when0 = models.DateField(blank=True, null=True)
+    o2_fs_when1 = models.DateField(blank=True, null=True)
+    o2_fs_when2 = models.DateField(blank=True, null=True)
+    o2_fs_when3 = models.DateField(blank=True, null=True)
+    o2_fs_when4 = models.DateField(blank=True, null=True)
+    o2_fs_when5 = models.DateField(blank=True, null=True)
+    o2_fs_when6 = models.DateField(blank=True, null=True)
+    o2_fs_when7 = models.DateField(blank=True, null=True)
+    o2_fs_when8 = models.DateField(blank=True, null=True)
+    o2_fs_when9 = models.DateField(blank=True, null=True)
+    o2_fs_cmts = models.TextField(blank=True, verbose_name='Comments')
 
     additional_title = ''
-    additional_cmts = models.TextField(blank=True)
+    additional_cmts = models.TextField(blank=True, verbose_name='Additional comments')
 
     def sections_with_values(self):
         sections = super().sections()
@@ -294,7 +343,7 @@ O2 CL slope of 0.01, such a slope should be near 0 when no nonlinearity is prese
                         continue
                     elif key == 'whens':
                         for ifield, (startfield, endfield) in enumerate(fieldset):
-                            fieldset[ifield] = self._format_date_range(startfield.name, endfield.name)
+                            fieldset[ifield] = self._format_date_range(startfield.name, endfield.name if endfield is not None else None)
                         question['whens'] = [w for w in fieldset if w]
                     else:
                         for ifield, field in enumerate(fieldset):
@@ -309,8 +358,20 @@ O2 CL slope of 0.01, such a slope should be near 0 when no nonlinearity is prese
 
     def _format_date_range(self, start_key, end_key):
         start = getattr(self, start_key, None)
+        if end_key is None:
+            # Handle single date cases (like for HCl VSF) where we specify single dates instead of ranges.
+            # If start is None, that means that the user did not provide a value for this date, so return
+            # None. Otherwise, just format the date.
+            if start is None:
+                return None
+            else:
+                return start.strftime('%Y-%m-%d')
+
         end = getattr(self, end_key, None)
 
+        # Handle paired date cases (i.e. beginning and ends). If both are None, assume no value given. If
+        # one is None, then assume that is an open-ended side to the range. Otherwise, just print the
+        # date range.
         if start is None and end is None:
             return None
 
