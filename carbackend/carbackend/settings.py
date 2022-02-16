ac99970aec1f2ef7d4f1215d200beb4766a5168f
Joshua Laughner <jlaugh@caltech.edu>
2022-02-16 11:03:34 -0800

    Added error logging when DEBUG=False. Form warns about unicode characters.

--- carbackend/carbackend/settings.py
+++ carbackend/carbackend/settings.py
@@ -62,6 +62,73 @@ elif VM == Vms.TCCONDATA:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+if VM == Vms.PARKFALLS or VM == Vms.TCCONDATA:
+    error_logger = {
+        'level': 'ERROR',
+        'filters': ['require_debug_false'],
+        'class': 'logging.FileHandler',
+        'filename': '/var/log/django/errors.log',
+        'formatter': 'errors'
+    }
+else:
+    error_logger = {
+        'level': 'ERROR',
+        'filters': ['require_debug_false'],
+        'class': 'logging.StreamHandler',
+    }
+
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse',
+		},
+		'require_debug_true': {
+			'()': 'django.utils.log.RequireDebugTrue',
+		},
+	},
+	'formatters': {
+		'django.server': {
+			'()': 'django.utils.log.ServerFormatter',
+			'format': '[%(server_time)s] %(message)s',
+		},
+                'errors': {
+                    'format': '\n\n=== %(asctime)s ===\n\n%(message)s'
+                }
+	},
+	'handlers': {
+		'console': {
+			'level': 'INFO',
+			'filters': ['require_debug_true'],
+			'class': 'logging.StreamHandler',
+		},
+		'console_debug_false': error_logger,
+		'django.server': {
+			'level': 'INFO',
+			'class': 'logging.StreamHandler',
+			'formatter': 'django.server',
+		},
+		'mail_admins': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'django.utils.log.AdminEmailHandler'
+		}
+	},
+	'loggers': {
+		'django': {
+			'handlers': ['console', 'console_debug_false', 'mail_admins'],
+			'level': 'INFO',
+		},
+		'django.server': {
+			'handlers': ['django.server'],
+			'level': 'INFO',
+			'propagate': False,
+		}
+	}
+}
+
+
 ALLOWED_HOSTS = ['parkfalls.gps.caltech.edu', '131.215.65.68', 'tccondata.gps.caltech.edu', 'tccondata.org']
 
 
