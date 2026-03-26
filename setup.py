80896594c5eba2c9e50c7d6a2016abb15db1db15
Joshua Laughner <jlaugh@caltech.edu>
2026-03-25 17:49:57 -0700

    Converted to pyproject.toml setup format

--- setup.py
+++ setup.py
@@ -1,17 +1,2 @@
 from setuptools import setup
-
-if __name__ == '__main__':
-    setup(
-            name='tccon_qc_email',
-            description='Email QA/QC plots of TCCON data to GGGBugs',
-            author='Sebastien Roche & Joshua Laughner',
-            author_email='sebastien.roche@mail.utoronto.ca; jlaugh@caltech.edu',
-            version='1.0.0',
-            url='https://github.com/WennbergLab/tccon-qc-email',
-            install_requires=[
-                    'tomli>=1.0.4',
-                    'requests',
-                ],
-            packages=['qc_email'],
-            python_requires='>=3.7'
-    )
+setup()
